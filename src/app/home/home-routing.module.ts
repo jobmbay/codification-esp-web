b9604a65f8794a79fe93751973ee782a42c9dc7c
youssou <ziggyou12@gmail.com>
2018-01-16 21:25:47 +0000

    Configuration routing Inscription

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,9 +3,11 @@
  */
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from "./home.component";
-import {DashboardComponent} from "./dashboard/dashboard.component";
+import { HomeComponent} from "./home.component";
+import { DashboardComponent} from "./dashboard/dashboard.component";
 import { CodificationComponent } from "./codification/codification.component";
+import { InscriptionComponent } from './inscription/inscription.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
 
 const homeRoutes: Routes = [
   {
@@ -13,7 +15,8 @@ const homeRoutes: Routes = [
     component: HomeComponent,
     children: [
       { path:'dashboard', component: DashboardComponent},
-      { path:'codification', component: CodificationComponent}
+      { path:'codification', component: CodificationComponent},
+      { path:'inscription', component: InscriptionComponent}
       
     ]
   }
