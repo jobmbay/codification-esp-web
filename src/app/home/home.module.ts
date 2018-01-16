fff1bd315266f7363573ecb70b0cb70ad4ca6ee4
youssou <ziggyou12@gmail.com>
2018-01-16 21:12:09 +0000

    Generation des components Inscription et Statistique

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import {HomeRoutingModule} from "./home-routing.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CodificationComponent } from './codification/codification.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { StatistiqueComponent } from './statistique/statistique.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [DashboardComponent, CodificationComponent]
+  declarations: [DashboardComponent, CodificationComponent, InscriptionComponent, StatistiqueComponent]
 })
 export class HomeModule { }
