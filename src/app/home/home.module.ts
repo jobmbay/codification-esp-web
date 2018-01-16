fb84b4374f73e24a0cf6c1a9746d7cc63ec39a9a
youssou <ziggyou12@gmail.com>
2018-01-16 08:53:17 +0000

    Generation du component codification

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {HomeRoutingModule} from "./home-routing.module";
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { CodificationComponent } from './codification/codification.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, CodificationComponent]
 })
 export class HomeModule { }
